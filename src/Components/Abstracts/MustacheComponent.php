0317451611fa04f3650e4cfa1816c232e0a5e010
Piotr Nowiński <piotr.nowinski@finiata.com>
2022-04-01 21:10:45 +0200

    MustacheComponent: add empty configureProps method

--- src/Components/Abstracts/MustacheComponent.php
+++ src/Components/Abstracts/MustacheComponent.php
@@ -2,6 +2,7 @@
 
 namespace Wpce\Components\Abstracts;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Wpce\Components\Abstracts\DefaultComponent;
 use Wpce\Content\Template;
 
@@ -12,11 +13,22 @@ use Wpce\Content\Template;
 abstract class MustacheComponent extends DefaultComponent {
 
   /**
-    * Gets component's HTML code
-    *
-    * @param array $options
-    * @return string|null
-    */
+   * Allows configuration of required props using OptionsResolver.
+   *
+   * Implemented here as an empty function to allow creation of
+   * propless mustache components. Overwrite as needed in child classes.
+   *
+   * @param array $options
+   * @return string|null
+   */
+  protected function configureProps(OptionsResolver $resolver, array $props) {}
+
+  /**
+   * Gets component's HTML code
+   *
+   * @param array $options
+   * @return string|null
+   */
   public function getHtml() {
     return Template::renderComponent($this->childRef->getShortName(), dirname($this->childRef->getFileName()), $this);
   }
