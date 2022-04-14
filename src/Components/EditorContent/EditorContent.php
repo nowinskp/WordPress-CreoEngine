bd282a4fd5db1951012ad68367fe9861236d538f
Piotr Nowiński <piotr.nowinski@finiata.com>
2022-04-14 20:12:33 +0200

    Add EditorContent component

---
+++ src/Components/EditorContent/EditorContent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wpce\Components\Button;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Wpce\Components\Abstracts\MustacheComponent;
+
+class EditorContent extends MustacheComponent {
+
+  /**
+   * @param array $props
+   * @property string 'content' HTML content of WP WYSIWYG editor
+   *
+   * @return string
+   */
+  protected function configureProps(OptionsResolver $resolver, array $props) {
+    $resolver->setDefaults([
+      'content' => null,
+    ]);
+  }
+
+  protected function parseProps(array $props) {
+    $this->content = apply_filters('the_content', $this->content);
+  }
+
+}
