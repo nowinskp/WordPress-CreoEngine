95625924dc8853c241ea887b2e76aa25f8d2a68e
Piotr Nowiński <p.nowinski@creoignis.com>
2023-02-20 19:56:15 +0100

    Add WordPress/Utils/Helpers::getUrlToCurrentThemeDir

--- src/WordPress/Utils/Helpers.php
+++ src/WordPress/Utils/Helpers.php
@@ -19,4 +19,20 @@ class Helpers {
     return $imageData ? $imageData[0] : null;
   }
 
+  /**
+   * Returns URL to current directory in WordPress theme dir.
+   *
+   * @param string $currentDir current dir path, usually the value of __DIR__
+   * @return string
+   */
+  static function getUrlToCurrentThemeDir(string $currentDir) {
+    $themeDirName = wp_get_theme()->get_template();
+    $themeUrl = get_bloginfo('template_url');
+    $themeDirPath = 'themes/'.$themeDirName;
+    $pathFromRoot = substr($currentDir, strlen($_SERVER['DOCUMENT_ROOT']));
+    $themeDirPos = strpos($pathFromRoot, $themeDirPath);
+    $pathToCurrentDirFromThemeRoot = substr($pathFromRoot, strlen($themeDirPath) + $themeDirPos);
+    return $themeUrl.$pathToCurrentDirFromThemeRoot;
+  }
+
 }
